d63c880e668a5afafbd0adf0ec5e4139cf9b5446
GMR Student <gmrstudent@outlook.com>
2022-06-15 16:44:32 -0700

    ye

---
+++ ImposterTrie/ImposterTrie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImposterTrie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
